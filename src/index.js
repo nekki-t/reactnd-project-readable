a6dc322cf2fc379b884365ae4e0b2454b3bb59a9
nekki-t <nekki.t.0317@gmail.com>
2017-10-25 19:24:11 +0900

    behavior settings

--- src/index.js
+++ src/index.js
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import './index.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import { URL } from './constants';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import createStore from "./createStore";
 
+import { initialize } from './actions/session';
+
 injectTapEventPlugin();
 
 const store = createStore();
+if (window.location.pathname !== URL.LOGIN) {
+  store.dispatch(initialize());
+}
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <MuiThemeProvider>
       <App/>
     </MuiThemeProvider>
